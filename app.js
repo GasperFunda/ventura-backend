8aa1dba7153859ab7c58cf9b1b9d545908815953
Gašper Funda Povše <gasper.fp@gmail.com>
2021-05-09 20:55:19 +0200

    Added connection to the db

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/userRoutes");
+var activitiesRouter = require("./routes/activityRoutes");
 
 var app = express();
 
-app.use(logger('dev'));
+var mongoDB =
+  "mongodb+srv://dbUser:dbUser@ventura.2ym7e.mongodb.net/Ventura?retryWrites=true&w=majority";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"));
+cors({ credentials: true, origin: true, exposedHeaders: "*" });
+app.use(cors());
+var MongoStore = require("connect-mongo");
+app.use(
+  session({
+    secret: "work hard",
+    resave: true,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: mongoDB }),
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/activities", activitiesRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,44 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	'username' : String,
-	'first_name' : String,
-	'last_name' : String,
-	'email' : String,
-	'password' : String,
-	'date_of_birth' : Date
+  username: String,
+  first_name: String,
+  last_name: String,
+  email: String,
+  password: String,
+  date_of_birth: Date,
 });
 
-module.exports = mongoose.model('user', userSchema);
+userSchema.statics.authenticate = function (username, password, callback) {
+  User.findOne({ username: username }).exec(function (err, user) {
+    if (err) {
+      return callback(err);
+    } else if (!user) {
+      var err = new Error("User not found");
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        return callback();
+      }
+    });
+  });
+};
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+var User = mongoose.model("user", userSchema);
+module.exports = User;
